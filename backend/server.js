620682a9118e7dc487724c8c6bc2a672775b11f5
MuhammadSanaullah123 <muhammed14336@gmail.com>
2023-12-15 17:25:35 +0500

    latest build

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 const connectDB = require("./config/db");
 const bcrypt = require("bcryptjs");
-
+const path = require("path");
 connectDB();
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
@@ -19,6 +19,21 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/document", require("./routes/api/document"));
 app.use("/api/conversation", require("./routes/api/conversation"));
 app.use("/api/invoice", require("./routes/api/invoice"));
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../frontend/build");
+
+app.use(express.static(buildPath));
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 const PORT = process.env.PORT || 5000;
 
